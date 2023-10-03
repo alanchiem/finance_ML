de9062be5668ceed908d0f1a02e0d0e4dc1f0ed6
alanchiem <chiemalan@gmail.com>
2023-10-02 17:32:41 -0700

    quant notes

---
+++ algotrading.py
@@ -0,0 +1,31 @@
+"""
+3. Key Concepts
+
+avoid look ahead bias
+
+tick data vs bar data
+
+QC Algorithm
+    Initialize
+    On Data
+
+Symbol Object
+    holds more info than ticker
+
+4. Handling Data
+
+TradeBars Equities Options and Futures
+    OHLC + Vol
+
+QuoteBars All Asset types
+    built by consolidating bid and ask
+    ask bar OHLC
+    bid bar OHLC
+    quote bar OHLC are midpoints from ask and bid bars
+
+Entry and Exit
+
+
+
+
+"""
